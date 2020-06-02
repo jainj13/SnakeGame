8b0ad65eec4d1f44dbb43f70e69b83bdbd24a53a
jainj13 <64269685+jainj13@users.noreply.github.com>
2020-06-02 09:13:55 -0400

    Update Game_set.java

--- Snake Game/src/Game_set.java
+++ Snake Game/src/Game_set.java
@@ -12,6 +12,8 @@
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
 
 
@@ -35,17 +37,69 @@ public class Game_set extends JPanel implements ActionListener{
     //array holds two snake obj
     Snake[] snakes = new Snake[2];
     
-    private void setup(){
-    
-    
-    
-    } 
+     private void set_Images(){
+
+         //Creating image icon and assinging each one to previously set variable
+        ImageIcon c = new ImageIcon("src/Char_image/coin.png");
+        coin = c.getImage();
+        
+        ImageIcon p = new ImageIcon("src/Char_image/Powerup.png");
+        powerup = p.getImage();
+        
+        ImageIcon sb1 = new ImageIcon("src/Char_image/Snake1.png");
+        Snake1 = sb1.getImage();
+        
+        ImageIcon sb2 = new ImageIcon("src/Char_image/Snake2.png");
+        Snake2 = sb2.getImage();
+        
+        ImageIcon b1 = new ImageIcon("src/Char_image/Body1.png");
+        body1 = b1.getImage();
+        
+        ImageIcon b2 = new ImageIcon("src/Char_image/Body2.png");
+        body2 = b2.getImage();
+        
+     }
     public Game_set (Snake player1, Snake player2){
         snakes[0] = player1;
         snakes[1] = player2;
         
     }
     
+    private void Init_board (){
+    }
+        
+    // variable to check for condition
+    private final int delay  = 50;
     
+    // variable to turn on and off
+    private boolean ingame = true;
     
-}
+    private Timer timer;
+   
+ //game set up method   
+        private void initGame(){
+        
+            //place snake but avoid being out of bounce
+            for (int z = 0; z < snakes[0].length; z++){
+                snakes[0].Xpos[z] = 50 - z *10;
+                snakes[0].Ypos[z] = 50;
+            }
+            
+            for (int z = 0; z < snakes[1].length; z++){
+                snakes[1].Xpos[z] = 400 - z *10;
+                snakes[1].Ypos[z] = 50;
+            }
+
+        
+        //coin drop STILL HAVE TO MAKE THE METHOD FOR IT
+        
+        ingame = true;
+        
+        timer = new Timer(delay, this);
+        timer.start();
+        //Start the game and timer
+    }
+    
+    }
+    
+
